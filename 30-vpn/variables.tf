1ccd3d397872878e5b9dc80b38fecaecc0b69cd3
shanthi <shanbojja@gmail.com>
2025-07-02 09:29:06 +0530

    ansible-terraform

--- 30-vpn/variables.tf
+++ 30-vpn/variables.tf
@@ -5,3 +5,11 @@ variable "project" {
 variable "environment" {
     default = "dev"
 }
+
+variable "zone_id" {
+    default = "Z07765362AYDJ93CC402A"
+}
+
+variable "zone_name" {
+    default = "daws-shankran.site"
+}
