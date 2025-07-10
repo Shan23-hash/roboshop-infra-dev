174e08976ec94b8ca8ec9cc6ecb38644cb6750cc
shanthi <shanbojja@gmail.com>
2025-07-10 23:40:07 +0530

    terraform

---
+++ 60-catalogue/catalogue.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+component=$1
+dnf install ansible -y
+<<<<<<< HEAD
+ansible-pull -U https://github.com/daws-84s/ansible-roboshop-roles-tf.git -e component=$1 -e env=$2 main.yaml
+=======
+ansible-pull -U https://github.com/Shan23-hash/ansible-roboshop-roles-tf.git  -e component=$1 -e env=$2 main.yaml
+>>>>>>> 0384568 (terraform)
---
+++ 70-frontend-alb/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z03468932B7WI1KW96CYF"
+}
+
+variable "zone_name" {
+    default = "daws-shankran.site"
+}
