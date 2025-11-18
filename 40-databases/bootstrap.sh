f79e667228d30b7fc30db8463d1d7988667c9472
shanthi <shanbojja@gmail.com>
2025-11-19 01:33:04 +0530

    terraform

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -2,8 +2,4 @@
 
 component=$1
 dnf install ansible -y
-<<<<<<< HEAD
-ansible-pull -U hhttps://github.com/Shan23-hash/ansible-roboshop-roles-tf.git -e component=$1 -e env=$2 main.yaml
-=======
-ansible-pull -U https://github.com/Shan23-hash/ansible-roboshop-roles-tf.git -e component=$1 -e env=$2 main.yaml
->>>>>>> 0384568 (terraform)
+ansible-pull -U https://github.com/daws-84s/ansible-roboshop-roles-tf.git -e component=$1 -e env=$2 main.yaml
