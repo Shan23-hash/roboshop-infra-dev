61d5799e3bfa279d470de5557d66bc3f87e291c2
shanthi <shanbojja@gmail.com>
2025-11-19 22:04:42 +0530

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -156,7 +156,11 @@ resource "terraform_data" "rabbitmq" {
 
 resource "aws_route53_record" "mongodb" {
   zone_id = var.zone_id
+<<<<<<< HEAD
   name    = "mongodb-${var.environment}.${var.zone_name}" #mongodb-dev.daws84s.site
+=======
+  name    = "mongodb-${var.environment}.${var.zone_name}" #mongodb-dev.daws-shankran.site
+>>>>>>> e2b8850 (terraform)
   type    = "A"
   ttl     = 1
   records = [aws_instance.mongodb.private_ip]
