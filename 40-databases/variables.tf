9400cfcec0395e0e313729e0837e9e333927c8ae
shanthi <shanbojja@gmail.com>
2026-08-30 17:57:32 +0530

    Terraform

--- 40-databases/variables.tf
+++ 40-databases/variables.tf
@@ -7,9 +7,9 @@ variable "environment" {
 }
 
 variable "zone_id" {
-    default = "Z03969043AYWH3KNYE2A6"
+    default = "Z08868581OGQ5IOUCAXOU"
 }
 
 variable "zone_name" {
-    default = "daws-shankran.site"
+    default = "kranthi.store"
 }
