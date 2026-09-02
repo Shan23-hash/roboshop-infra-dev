2d133f910380b093a6ee97b1f18088445a79f033
shanthi <shanbojja@gmail.com>
2026-09-02 15:56:47 +0530

    Terraform

---
+++ 91-cdn/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z08868581OGQ5IOUCAXOU"
+}
+
+variable "zone_name" {
+    default = "kranthi.store"
+}
