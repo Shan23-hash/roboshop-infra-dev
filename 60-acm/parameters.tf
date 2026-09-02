e8a403e6425ac9038c1e93173a80a438e1fab834
shanthi <shanbojja@gmail.com>
2026-09-02 13:12:45 +0530

    Terraform

--- 60-acm/parameters.tf
+++ 60-acm/parameters.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "acm_certificate_arn" {
   name  = "/${var.project}/${var.environment}/acm_certificate_arn"
   type  = "String"
-  value = aws_acm_certificate.kranthi
+  value = aws_acm_certificate.kranthi.arn
 }
